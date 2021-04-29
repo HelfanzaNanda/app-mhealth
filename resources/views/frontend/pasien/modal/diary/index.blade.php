6533d867ad9cd8095a2a987fd553cfc2656004a0
HelfanzaNanda <elvannnd@gmail.com>
2021-04-29 11:52:16 +0700

    required form diary

--- resources/views/frontend/pasien/modal/diary/index.blade.php
+++ resources/views/frontend/pasien/modal/diary/index.blade.php
@@ -9,22 +9,22 @@
             <input type="hidden" name="id" value="{{ $data['id'] }}">
             <div class="form-group">
                 <label class="font-weight-500">Apa yang ibu rasakan hari ini ?</label>
-                <input type="text" name="info1" id="info1" class="form-control font-size-16 form-mhealth"
+                <input type="text" required name="info1" id="info1" class="form-control font-size-16 form-mhealth"
                     value="{{ $data['info1'] }}">
             </div>
             <div class="form-group">
                 <label class="font-weight-500">Apa yang ibu makan dan minum hari ini ?</label>
-                <input type="text" name="info2" id="info2" class="form-control font-size-16 form-mhealth"
+                <input type="text" required name="info2" id="info2" class="form-control font-size-16 form-mhealth"
                     value="{{ $data['info2'] }}">
             </div>
             <div class="form-group">
                 <label class="font-weight-500">Apakah ibu sudah minum obat / vitamin hari ini ?</label>
-                <input type="text" name="info3" id="info3" class="form-control font-size-16 form-mhealth"
+                <input type="text" required name="info3" id="info3" class="form-control font-size-16 form-mhealth"
                     value="{{ $data['info3'] }}">
             </div>
             <div class="form-group">
                 <label class="font-weight-500">Bagaimana kondisi janin ibu hari ini ?</label>
-                <input type="text" name="info4" id="info4" class="form-control font-size-16 form-mhealth"
+                <input type="text" required name="info4" id="info4" class="form-control font-size-16 form-mhealth"
                     value="{{ $data['info4'] }}">
             </div>
             <button class="btn btn-mhealth btn-block btn-pink text-white" type="submit" onclick="Save()">Simpan</button>
